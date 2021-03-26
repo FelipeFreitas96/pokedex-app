99afc4d63d20df7b3c3940145b7ced57d6d653e5
FelipeFreitas96 <felipe.samidare@hotmail.com>
2021-03-25 23:00:29 -0300

    chore(services): should be successful when pokemon exists

--- src/data/services/get-pokemon-data.ts
+++ src/data/services/get-pokemon-data.ts
@@ -1,9 +1,18 @@
+import {IPokemon} from '../../domain/entities/pokemon';
 import {IGetPokemonData} from '../../domain/usecase/get-pokemon-data';
 import {IApi} from '../protocols/api';
 
 export class GetPokemonData implements IGetPokemonData {
   constructor(private readonly pokemonApi: IApi) {}
-  execute(): string {
-    throw new Error('Method not implemented.');
+  async execute(id: number): Promise<IPokemon> {
+    const { data } = await this.pokemonApi.get(`/pokemon/${id}`);
+    const result: IPokemon = {
+      name: data.name,
+      type: {
+        primary: data.types[0].type.name,
+        secondary: data?.types[1]?.type?.name || null,
+      },
+    };
+    return result;
   }
 }
