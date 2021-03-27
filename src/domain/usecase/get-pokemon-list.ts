43e69cb9e1f15f729f6c61ef0551b1cafc4a530b
FelipeFreitas96 <felipe.samidare@hotmail.com>
2021-03-27 13:58:23 -0300

    refactor(domain): get pokemon list usecase

--- src/domain/usecase/get-pokemon-list.ts
+++ src/domain/usecase/get-pokemon-list.ts
@@ -1,9 +1,5 @@
 import { IPokemon } from "../entities/pokemon";
 
-export type GetPokemonDTO = {
-  [pokemonName: string]: IPokemon;
-}
-
 export interface IGetPokemonList {
-  execute(): Promise<{ [pokemonName: string]: IPokemon }>;
+  execute(): Promise<IPokemon[]>;
 }
