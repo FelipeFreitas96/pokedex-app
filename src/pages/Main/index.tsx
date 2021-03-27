9616dfafa4d8a6aeb21f8f6cd33913c0637729f7
FelipeFreitas96 <felipe.samidare@hotmail.com>
2021-03-27 14:01:06 -0300

    refactor(pages): display all pokemons in main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,28 +1,45 @@
 import React from 'react';
+import {SafeAreaView, ScrollView, View} from 'react-native';
 import PokemonCard from '../../components/PokemonCard';
-import {View} from 'react-native';
+import { AxiosAdapter } from '../../infra/axios-adapter';
+import { GetPokemonListService } from '../../data/services/get-pokemon-list';
 
 export default () => {
+  const [pokemonList, setPokemonList] = React.useState([]);
+
+  React.useEffect(() => {
+    (async () => {
+      setPokemonList(await renderPokemons());
+    })();
+  }, []);
+  
+  const renderPokemons = React.useCallback(async () => {
+    const instance = new AxiosAdapter();
+    const service = new GetPokemonListService(instance);
+    const pokemonList = await service.execute();
+    return pokemonList.map((pokemon, index) => {
   return (
+        <PokemonCard
+          key={`pokemon_${index}`}
+          name={pokemon.name}
+          types={{ primary: pokemon.type.primary, secondary: pokemon.type.secondary }}
+        />
+      );
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView>
     <View
       style={{
         flex: 1,
         flexWrap: 'wrap',
         flexDirection: 'row',
       }}>
-      <PokemonCard
-        name="Bulbasaur"
-        types={{primary: 'Grass', secondary: 'Poison'}}
-      />
-      <PokemonCard
-        name="Ivysaur"
-        types={{primary: 'Grass', secondary: 'Poison'}}
-      />
-      <PokemonCard
-        name="Venusaur"
-        types={{primary: 'Grass', secondary: 'Poison'}}
-      />
-      <PokemonCard name="Charmander" types={{primary: 'Fire'}} />
+        {pokemonList}
     </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
