0dea91edf84de4527a2c04ce7294dafbeebd389d
FelipeFreitas96 <felipe.samidare@hotmail.com>
2021-03-27 06:34:04 -0300

    chore(domain): add pokemons type

--- src/domain/entities/pokemon.ts
+++ src/domain/entities/pokemon.ts
@@ -1,3 +1,21 @@
+export const PokemonTypes = [
+  "bug",
+  "dragon",
+  "electric",
+  "fighting",
+  "fire",
+  "flying",
+  "ghost",
+  "grass",
+  "ground",
+  "ice",
+  "normal",
+  "poison",
+  "psychic",
+  "rock",
+  "water"
+];
+
 export type PokemonType = {
   primary: string;
   secondary: string;
