29bfe6eba66d72f8ba100da743ec6eee4f84ba6d
FelipeFreitas96 <felipe.samidare@hotmail.com>
2021-03-27 14:00:12 -0300

    refactor(services): get pokemon list service

--- src/data/services/get-pokemon-list.ts
+++ src/data/services/get-pokemon-list.ts
@@ -1,11 +1,12 @@
-import { PokemonTypes } from '../../domain/entities/pokemon';
-import {GetPokemonDTO, IGetPokemonList} from '../../domain/usecase/get-pokemon-list';
+import { IPokemon, PokemonTypes } from '../../domain/entities/pokemon';
+import { IGetPokemonList} from '../../domain/usecase/get-pokemon-list';
+import { capitalize } from '../../helper/capitalize';
 import { IApi } from '../protocols/api';
 
 export class GetPokemonListService implements IGetPokemonList {
   constructor(private readonly pokemonApi: IApi) {}
-  async execute(): Promise<GetPokemonDTO> {
-    const pokemonList: GetPokemonDTO = {};
+  async execute(): Promise<IPokemon[]> {
+    const pokemonList: IPokemon[] = [];
     for (const pokemonType of PokemonTypes) {
       const response = await this.pokemonApi.get(`https://pokeapi.co/api/v2/type/${pokemonType}`);
       if (!response ||
@@ -15,20 +16,21 @@ export class GetPokemonListService implements IGetPokemonList {
 
       for (const pokemonData of response.data.pokemon) {
         const { pokemon, slot } = pokemonData;
-        const pokemonId = Number([...pokemon.url.matchAll(/\/pokemon\/(.*)\//gm)][0][1]);
+        const regex = new RegExp('/pokemon/(.*)/', 'g');
+        const pokemonId = Number(regex.exec(pokemon.url)[1]);
         if (pokemonId > 151) {
           continue;
         }  
 
         if (pokemonList[pokemonId - 1] === undefined) {
           pokemonList[pokemonId - 1] = {
-            name: pokemon.name,
+            name: capitalize(pokemon.name),
             type: {},
           }
         }
 
         const pokemonTypeByCondition = (slot === 1 ? "primary" : "secondary");
-        pokemonList[pokemonId - 1].type[pokemonTypeByCondition] = pokemonType;
+        pokemonList[pokemonId - 1].type[pokemonTypeByCondition] = capitalize(pokemonType);
       }
     }
 
