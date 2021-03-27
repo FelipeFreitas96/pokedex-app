8fa39d7be96e0d6c9c9ca917d73cbdf6a220ec92
FelipeFreitas96 <felipe.samidare@hotmail.com>
2021-03-27 15:10:36 -0300

    refactor(test): improvement of test titles

--- src/helper/capitalize.spec.ts
+++ src/helper/capitalize.spec.ts
@@ -1,11 +1,11 @@
 import { capitalize } from "./capitalize"
 
 describe('Capitalize Function', () => {
-  it('should be capitalize first letter', () => {
+  it('should be return capitalized first letter', () => {
     const string = capitalize('any_string');
     expect(string).toBe('Any_string');
   })
-  it('should be return empty string when string param is null', () => {
+  it('should be return an empty string when string param is null', () => {
     const string = capitalize(null);
     expect(string).toBe('');
   })
