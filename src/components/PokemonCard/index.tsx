38644f8b4ce5bcd8c35208e8061364e10615e3ba
FelipeFreitas96 <felipe.samidare@hotmail.com>
2021-03-27 21:18:57 -0300

    feat(components): Pokemon card with pokemon portrait

--- src/components/PokemonCard/index.tsx
+++ src/components/PokemonCard/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { View, Dimensions } from 'react-native';
+import { SvgUri } from 'react-native-svg';
+import PokeballSVG from '../../resources/svg/pokeball-icon.svg';
 import PokemonCard, {IPokemonCardComponent} from './style';
 
 export default (_props: IPokemonCardComponent) => {
   const {types} = _props;
+  const windowWidth = Dimensions.get('window').width;
+  const pokemonSize = windowWidth * 0.25;
   return (
     <PokemonCard types={types}>
       <PokemonCard.Name>{_props.name}</PokemonCard.Name>
@@ -20,6 +25,19 @@ export default (_props: IPokemonCardComponent) => {
           </PokemonCard.Tag>
         )}
       </PokemonCard.Tags>
+      <View style={{ position: 'absolute', bottom: 10, right: 10, justifyContent: 'center', alignContent: 'center', width: pokemonSize, height: pokemonSize }}>
+        <PokeballSVG
+          width="100%"
+          height="100%"
+          fill="white"
+          style={{ position: 'absolute', opacity: 0.5 }}
+        />
+        <SvgUri
+          width="80%"
+          height="80%"
+          uri={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${_props.id}.svg`}
+        />
+      </View>
     </PokemonCard>
   );
 };
